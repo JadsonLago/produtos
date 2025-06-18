b393d3c0390a9ddc5d86220f98f65a875db5810c
JadsonLago <jadsonlago.consultoria@gmail.com>
2025-06-17 21:28:45 -0300

    feat: implementacao do ProdutoController

---
+++ src/main/java/com/pratica/produtos/controller/ProdutoController.java
@@ -0,0 +1,54 @@
+package com.pratica.produtos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pratica.produtos.model.Produto;
+import com.pratica.produtos.service.ProdutoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@Controller
+@RequestMapping("/produtos")
+public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public String listarTodo(Model model) {
+        model.addAttribute("produtos", produtoService.listarTodos());
+        return "produtos/lista";
+    }
+    
+        @GetMapping("/novo")
+    public String novoProdutoForm(Model model) {
+        model.addAttribute("produto", new Produto());
+        return "produtos/form";
+    }
+
+    @PostMapping("/salvar")
+    public String salvarProduto(@ModelAttribute Produto produto) {
+        produtoService.salvar(produto);
+        return "redirect:/produtos";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String editarProdutoForm(@PathVariable Long id, Model model) {
+        model.addAttribute("produto", produtoService.buscarPorId(id).orElseThrow(() -> new IllegalArgumentException("ID inválido:" + id)));
+        return "produtos/form";
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarProduto(@PathVariable Long id) {
+        produtoService.deletar(id);
+        return "redirect:/produtos";
+    }
+
+
+}
